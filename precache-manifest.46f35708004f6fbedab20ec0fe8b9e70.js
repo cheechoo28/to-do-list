db83c2997ffe3b7c8786ddf22e4a027325042ffd
Denis <den-282008@yandex.ru>
2021-02-25 00:53:58 +0300

    Updates

---
+++ precache-manifest.46f35708004f6fbedab20ec0fe8b9e70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b88cbb7c07f205c1a873775f39ff01c5",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "42819492fab1ed7f9056",
+    "url": "/to-do-list/static/css/main.b7023c28.chunk.css"
+  },
+  {
+    "revision": "0724bab17fadc637d639",
+    "url": "/to-do-list/static/js/2.31087c5d.chunk.js"
+  },
+  {
+    "revision": "d2266803016540034565984c8bb59069",
+    "url": "/to-do-list/static/js/2.31087c5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42819492fab1ed7f9056",
+    "url": "/to-do-list/static/js/main.2eeedc23.chunk.js"
+  },
+  {
+    "revision": "6c26975b57d8d730a898",
+    "url": "/to-do-list/static/js/runtime-main.59ffd6f3.js"
+  }
+]);
